fc159087a95764ab0c9900ec5ca1a005ef2ec576
mafermugica <mafer.mugica5@gmail.com>
2026-01-25 11:59:57 -0600

    Add function gethumanchoice

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function getComputerChoice (option){
     return option[Math.floor(Math.random() * option.length)];
 }
 console.log(getComputerChoice(option));
+
+//function that takes the user choice and returns it
+function geHumanChoice (){
+    let input = prompt('Select rock, paper or scissors');
+    return input;
+
+}
